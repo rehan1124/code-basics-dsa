42ecbf69ccd4f7046629772a956e1bf37ef975f9
rehan.11.24@gmail.com <rehan.11.24@gmail.com>
2022-06-08 20:21:22 +0530

    Completed Stack implementation

--- stack.py
+++ stack.py
@@ -8,7 +8,46 @@ Using collections.deque
 
 from collections import deque
 
-stack = deque()
-stack.append("A")
-stack.append("B")
-print(stack)
+
+# stack = deque()
+# stack.append("A")
+# stack.append("B")
+# print(stack)
+
+
+class Stack:
+    def __init__(self):
+        self.container = deque()
+
+    def size(self):
+        return len(self.container)
+
+    def is_empty(self):
+        return self.size() == 0
+
+    def push(self, data):
+        self.container.append(data)
+
+    def pop(self):
+        return self.container.pop()
+
+    def peek(self):
+        return self.container[-1]
+
+
+if __name__ == "__main__":
+    s1 = Stack()
+    # --- Is Stack empty ---
+    print(s1.is_empty())  # True
+    # --- Stack size ---
+    print(s1.size())  # 0
+    # --- Add elements to Stack ---
+    s1.push("A")
+    s1.push("B")
+    s1.push("C")
+    print(s1.container)  # deque(['A', 'B', 'C'])
+    # --- Remove elements from Stack ---
+    print(s1.pop())  # C
+    print(s1.container)  # deque(['A', 'B'])
+    # --- Check element on top of Stack ---
+    print(s1.peek())  # B
