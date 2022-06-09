722f8e93aca880b844245627c59dbbc8a26dae18
rehan.11.24@gmail.com <rehan.11.24@gmail.com>
2022-06-09 11:53:01 +0530

    Tree start

---
+++ tree.py
@@ -0,0 +1,10 @@
+"""
+Tree (General Tree)
+Tree data structure is used to represent hierarchical data such as organization hierachy,
+product categories, geographic locations etc.
+
+Root node -> Nodes -> Leaf nodes
+Level 0 -> Level 1 -> Level 2
+
+Reference: https://www.youtube.com/watch?v=4r_XR9fUPhQ&list=PLeo1K3hjS3uu_n_a__MI_KktGTLYopZ12&index=9
+"""
