9a0ac02009505d0b39265486624114796ccf3e92
rehan.11.24@gmail.com <rehan.11.24@gmail.com>
2022-06-11 12:29:14 +0530

    Binary search tree initial commit

--- binary_tree1.py
+++ binary_tree1.py
@@ -20,3 +20,37 @@ Pre order traversal ->  Parent/root node, Left nodes, right node
 Post order traversal -> Left nodes, right nodes, Parent/root node
 
 """
+
+
+class BinaryTree:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def add_child(self, data):
+        """
+        Adds 'data' as 'BinaryTree' node
+        :param data:
+        :return:
+        """
+        if data == self.data:
+            # Check if data to be added is same as present node data
+            return
+
+        if data < self.data:
+            # Check if data to be added is smaller than present node data
+            if self.left is None:
+                # Add only if left of parent node is None
+                self.left = BinaryTree(data)
+            else:
+                # If left of parent node is not None, make recursive calls
+                self.add_child(data)
+        else:
+            # Check if data to be added is larger than present node data
+            if self.right is None:
+                # Add only if right of parent node is None
+                self.right = BinaryTree(data)
+            else:
+                # If right of parent node is not None, make recursive calls
+                self.add_child(data)
